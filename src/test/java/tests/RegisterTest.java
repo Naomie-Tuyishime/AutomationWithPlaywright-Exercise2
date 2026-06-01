9abce207beae3ef0a8e53f3f12187e413629e530
Naomie-Tuyishime <tuyishimenaome58@gmail.com>
2026-06-01 11:50:22 +0200

    adding negative testcase

--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import tests.base.BaseTest;
+import utils.ConfigLoader;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class RegisterTest extends BaseTest {
 
@@ -17,6 +20,28 @@ public class RegisterTest extends BaseTest {
         registrationActions.navigatingToRegisterPage();
         registrationActions.fillRegistrationForm("abc","tz", "tz@gmail.com","Kigali@123");
         registrationActions.clickRegistration();
+        assertThat(page).hasURL(ConfigLoader.get("baseUrl") + "/home");
+
 
     }
+    @ParameterizedTest
+    @ValueSource(strings = {"chromium"})
+    public void TestRegisteringWithEmptyPasswordInput(String browserName) {
+        setUp(browserName);
+        this.registrationActions = new RegistrationActions(page);
+
+        registrationActions.navigatingToRegisterPage();
+
+        registrationActions.fillRegistrationForm(
+                "nnn",
+                "nn",
+                "bbb@gmail.com",""
+
+        );
+
+        registrationActions.clickRegistration();
+
+        assertThat(page).hasURL(ConfigLoader.get("baseUrl") + "/register");
+    }
+
 }
